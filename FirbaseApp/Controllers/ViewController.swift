1af165c3c1f47b60054c8cce038573e8da9250c4
Facundo <faquidg@hotmail.com>
2026-02-05 20:47:45 +0100

    feat: add auth funcionality with firbase

--- FirbaseApp/Controllers/ViewController.swift
+++ FirbaseApp/Controllers/ViewController.swift
@@ -5,13 +5,49 @@
 //  Created by Tardes on 5/2/26.
 //
 
+import FirebaseAuth
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var userNameInput: UITextField!
+    @IBOutlet weak var passwordInput: UITextField!
+    @IBOutlet weak var signUpButton: UIButton!
+    @IBOutlet weak var signInButton: UIButton!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+
+    @IBAction func singUp(_ sender: Any) {
+        Auth.auth().createUser(
+            withEmail: userNameInput.text ?? "",
+            password: passwordInput.text ?? ""
+        ) { [unowned self] authResult, error in
+            if let error = error {
+                print("Error creating account: \(error)")
+                return
+            }
+
+            print("Account created successfully!")
+            performSegue(withIdentifier: "navigateToHome", sender: nil)
+        }
+    }
+
+    @IBAction func singIn(_ sender: Any) {
+        Auth.auth().signIn(
+            withEmail: userNameInput.text ?? "",
+            password: passwordInput.text ?? ""
+        ) { [unowned self] authResult, error in
+            if let error = error {
+                print("Error sing in: \(error)")
+                return
+            }
+
+            print("Signed in successfully!")
+            performSegue(withIdentifier: "navigateToHome", sender: nil)
+        }
     }
 
 }
