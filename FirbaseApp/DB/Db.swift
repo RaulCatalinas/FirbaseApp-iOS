71ab9cd524d35b0ec951628975cf81f907093405
Facundo <faquidg@hotmail.com>
2026-02-10 16:09:08 +0100

    chore: add missing Db file

--- FirbaseApp/DB/Db.swift
+++ FirbaseApp/DB/Db.swift
@@ -9,12 +9,12 @@ import FirebaseFirestore
 
 final class Db {
     static let shared = Db()
+    private let db = Firestore.firestore()
 
     private init() {}
 
     func saveUser(_ user: UserEntity) {
         do {
-            let db = Firestore.firestore()
 
             try db.collection("Users").document(user.id).setData(from: user)
         } catch {
@@ -24,13 +24,11 @@ final class Db {
 
     func existUser(withId id: String) async -> Bool {
         do {
-            let db = Firestore.firestore()
-
             let docRef = db.collection("Users").document(id)
 
-            try await docRef.getDocument()
+            let doc = try await docRef.getDocument()
 
-            return true
+            return doc.exists
         } catch {
             print("Error getting document: \(error)")
 
