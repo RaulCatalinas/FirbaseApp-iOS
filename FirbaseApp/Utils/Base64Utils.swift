c542bdbdd1b61f9fb8e92f3f0a2c24c416b0a4d8
Facundo <faquidg@hotmail.com>
2026-02-11 19:45:22 +0100

    feat: add logic to encode and decode an image in base 64.

--- FirbaseApp/Utils/Base64Utils.swift
+++ FirbaseApp/Utils/Base64Utils.swift
@@ -5,4 +5,24 @@
 //  Created by Tardes on 11/2/26.
 //
 
-import Foundation
+import UIKit
+
+extension UIImage {
+    var base64: String? {
+        self.jpegData(compressionQuality: 1)?.base64EncodedString()
+    }
+}
+
+extension String {
+    var imageFromBase64: UIImage? {
+        guard
+            let imageData = Data(
+                base64Encoded: self,
+                options: .ignoreUnknownCharacters
+            )
+        else {
+            return nil
+        }
+        return UIImage(data: imageData)
+    }
+}
