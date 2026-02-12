d21560e0f5052acf52de5cd4d4fa35344adecdd0
Facundo <faquidg@hotmail.com>
2026-02-12 20:44:54 +0100

    chore: add DetailsRecipeScreen storyboard file and navigation (UI pending)

---
+++ FirbaseApp/Controllers/DetailsRecipeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DetailsRecipeViewController.swift
+//  FirbaseApp
+//
+//  Created by Tardes on 12/2/26.
+//
+
+import UIKit
+
+class DetailsRecipeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
